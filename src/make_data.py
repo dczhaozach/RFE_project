1d83bff0a8360c89db86aea30d771adedad2f71e
Dongchen Zhao <dczhao@utexas.edu>
2022-05-26 15:24:58 -0500

    Added life path data and regression

--- src/make_data.py
+++ src/make_data.py
@@ -18,7 +18,10 @@ pd.options.mode.use_inf_as_na = True
 
 def data_load(config):
     """
-    data_load function that load raw Regdata and BDS data
+    data_load function that
+        - load raw Regdata and BDS data
+        - create Entry at sector level
+        - create simple measurement (firm exit)
 
     Args:
         config [str]: config file
@@ -57,9 +60,10 @@ def data_load(config):
     # change variable types
     df['firms'] = pd.to_numeric(df['firms'], errors='coerce', downcast=None)
     df['death'] = pd.to_numeric(df['firmdeath_firms'], errors='coerce', downcast=None)
-
+    df['death_rate'] = df['death']/df['firms'] 
+    
     # aggregate entry by sector
-    # creat aggregation for entry and death measure
+    # create aggregation for entry and death measure
 
     df_age = pd.DataFrame()
     df_age['entry_whole'] = df.loc[df['fage'] == 'a) 0', :].groupby(['year', 'sector'])['firms'].sum()
@@ -68,22 +72,26 @@ def data_load(config):
     # df_age['death'] = df.loc[df['fage'] != 'a) 0', :].groupby(['year', 'sector'])['firmdeath_firms'].sum()
 
     return df, regdata, gdp, df_age
- 
-def data_merge(config):
+
+def data_cohort(config):
     """
-    data_merge function merge Regdata sector GDP, and BDS data
+    data_cohort function
+        - merge regulation, gdp, entry at the time of entering for each cohort and observed years
+        - create log measurements
+        
     Args:
         config [str]: config file
     Returns:
-        Merged data
+        Merged cohort data
     """
+    
     ####################
     # Load data
     ####################
     df, regdata, gdp, df_age= data_load(config)
 
     ####################
-    # Merge Datasets
+    # Merge Datasets at observed years
     ####################
 
     # merge df and regulation levels at different industry levels
@@ -105,23 +113,6 @@ def data_merge(config):
     # merge with Sector Entry and
     df = df.merge(df_age, how = "left", left_on=["year", "sector"], right_on=["year", "sector"])
     
-    return df
-
-def data_cohort(config):
-    """
-    data_cohort function merge regulation, gdp, entry at the time of entering for each cohort
-    Args:
-        config [str]: config file
-    Returns:
-        Merged cohort data
-    """
-    ####################
-    # Load raw and merged data
-    ####################
-
-    df = data_merge(config)
-    df_raw, regdata, gdp, df_age = data_load(config)
-
     ####################
     # Define coarse age groups and cohort year variables
     ####################
@@ -156,6 +147,8 @@ def data_cohort(config):
                                 errors="raise")
                                 
         df = df.drop(columns='sector_reg')
+        
+        # create log variables
         df[f'log_restriction_1_{naics}_cohort'] = np.log(df[f'industry_restrictions_1_0_{naics}_cohort'])
         df[f'log_restriction_2_{naics}_cohort'] = np.log(df[f'industry_restrictions_2_0_{naics}_cohort'])
 
@@ -173,10 +166,106 @@ def data_cohort(config):
     df = df.merge(df_age_new, 
         how = "left", left_on=["cohort", 'sector'], right_on=["cohort", "sector"])
 
+    # create some variables
+    with warnings.catch_warnings(): # suppress log zero warnings
+        warnings.simplefilter('ignore')
+        df['log_gdp'] = np.log(df['gdp'])
+        df["log_gdp_cohort"] = np.log(df["gdp_cohort"])
+
+        df['entry_rate_whole'] = df['entry_whole']/df['incumbents_whole']  
+        df["entry_rate_whole_cohort"] = df["entry_whole_cohort"]/df["incumbents_whole_cohort"]       
+
     return df
 
+def data_life_path(config):
+    """
+    data_life_path function 
+        - merges regulation, gdp, entry on each cohort life path
+        - create log and flow measure measurements on each cohort life path
+        
+    Args:
+        config [str]: config file
+    Returns:
+        Merged cohort data
+    """
+    ####################
+    # Load raw and merged data
+    ####################
 
-def data_clean(config_file, return_data="N"):
+    df, regdata, gdp, df_age = data_load(config)
+
+    ####################
+    # Define coarse age groups and cohort year variables
+    ####################
+
+    # define age groups
+    df = df.sort_values(by=['year', 'sector', 'fage'])
+    i = 0
+    for fage in df.fage.unique():
+        df.loc[df.fage == fage, 'age_grp_dummy'] = i
+        i = i + 1
+
+    df.loc[df.age_grp_dummy >= 7, 'age_grp_dummy'] = 7
+    
+    ####################
+    # Merge variables at life path
+    ####################
+    
+    # rename the variables to merge (prevent from conflicts and harmonize data types)
+    regdata = regdata.rename(columns={"year": "year_reg"})
+    gdp = gdp.rename(columns={"year": "year_gdp"})
+    gdp["year_gdp"] = pd.to_numeric(gdp["year_gdp"]).astype(np.int64)
+    df_age_new = df_age.rename(columns={"year": "year_entry"})
+    df_age_new = df_age_new[["year_entry", "sector", "entry_whole", "incumbents_whole"]]
+     
+    naics = 2
+    sector_name = f"sector_{naics}"
+    df[sector_name] = df['sector'].astype(str).str.slice(0,naics)
+    df[sector_name] = pd.to_numeric(df[sector_name])
+    
+    # merge by ages
+    for age in range(0, 6):
+        
+        # create lag year data
+        df.loc[df.age_grp_dummy <= 5, f"L_{age}_year"] = \
+            df.loc[df.age_grp_dummy <= 5, 'year'] - age
+
+        ####################
+        # Merge with cohort year variables
+        ####################
+
+        df = df.merge(regdata, how = "left", left_on=[f"L_{age}_year", sector_name], right_on=["year_reg", "sector_reg"])
+        df = df.rename(columns={"industry_restrictions_1_0": f"L_{age}_industry_restrictions_1_0", 
+                                "industry_restrictions_2_0": f"L_{age}_industry_restrictions_2_0"}, 
+                                errors="raise")
+                                
+        df = df.drop(columns = {'sector_reg', "year_reg"})
+
+        df = df.merge(gdp, how = "left", left_on=[f"L_{age}_year", "sector_2"], right_on=["year_gdp", "sector_2"])
+        df = df.rename(columns={"gdp": f"L_{age}_gdp"}, 
+                                errors="raise")
+        df = df.drop(columns = {"year_gdp"})
+        
+        # merge the entry rate
+        df = df.merge(df_age_new, 
+            how = "left", left_on=[f"L_{age}_year", 'sector'], right_on=["year_entry", "sector"])
+        df = df.rename(columns={"entry_whole": f"L_{age}_entry_whole",
+                                "incumbents_whole": f"L_{age}_incumbents_whole"})
+        df = df.drop(columns = {"year_entry"})
+        
+        # create log variables
+        with warnings.catch_warnings(): # suppress log zero warnings
+            warnings.simplefilter('ignore')
+            df[f'L_{age}_log_restriction_1_0'] = np.log(df[f"L_{age}_industry_restrictions_1_0"])
+            df[f'L_{age}_log_restriction_2_0'] = np.log(df[f"L_{age}_industry_restrictions_2_0"])
+            
+            df[f'L_{age}_log_gdp'] = np.log(df[f"L_{age}_gdp"])
+            
+            df[f'L_{age}_entry_rate_whole'] = df[f'L_{age}_entry_whole']/df[f'L_{age}_incumbents_whole']
+            
+    return df
+
+def data_output(config_file):
     """
     data_clean function clean and create the final dataset
     Args:
@@ -184,50 +273,28 @@ def data_clean(config_file, return_data="N"):
     Returns:
         Final data
     """
-    ####################
-    # Load data and config_file
-    ####################
 
-    # load config from config file
+    ####################
+    # Load data and config file
+    ####################
     config = parse_config(config_file)
     
-    df = data_cohort(config)
-
-    ####################
-    # Create Variables for USe
-    ####################
-
-    # create some variables
-    with warnings.catch_warnings(): # supress log zero warnings
-        warnings.simplefilter('ignore')
-        df['log_entry_whole'] = np.log(df['entry_whole'])
-        df['log_death'] = np.log(df['death'])
-        df['log_incumbents_whole'] = np.log(df['incumbents_whole'])
-        df['entry_rate_whole'] = df['entry_whole']/df['incumbents_whole'] 
-        df['death_rate_age'] = df['death']/df['firms'] 
-        df['log_gdp'] = np.log(df['gdp'])
-        df["entry_rate_whole_cohort"] = df["entry_whole_cohort"]/df["incumbents_whole_cohort"]
-        df["log_gdp_cohort"] = np.log(df["gdp_cohort"])
+    df_cohort = data_cohort(config)
+    df_life_path = data_life_path(config)
     
     # store cleaned dataset
     cleaned_data_path = Path(config["make_data"]["cleaned_data_path"])
-    df.to_csv(Path.cwd()/cleaned_data_path)
-
-    match return_data:
-        case "N":
-            pass
-        case "Y":
-            return df
-        case _:
-            raise Exception("Select 'Y' to output dataframe or 'N' to omit it.")
+    df_cohort.to_csv(Path.cwd()/cleaned_data_path/"cohort.csv")
+    df_life_path.to_csv(Path.cwd()/cleaned_data_path/"life_path.csv")
+    
 
 @click.command()
 @click.argument("config_file", type=str, default="src/config.yaml") 
-def data_clean_cmd(config_file):
+def data_output_cmd(config_file):
     """
-    data_clean_cmd use to generate cmd commend
+    data_output_cmd use to generate cmd commend
     """
-    data_clean(config_file)
+    data_output(config_file)
 
 if __name__ == "__main__":
-    data_clean_cmd()
+    data_output_cmd()
--- src/model.py
+++ src/model.py
@@ -8,6 +8,7 @@ import logging
 import click
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 from pathlib import Path
 from linearmodels.panel import PanelOLS
 
@@ -17,26 +18,22 @@ from src.utility import parse_config
 pd.options.mode.use_inf_as_na = True
 
 
-@click.command()
-@click.argument("config_file", type=str, default="src/config.yaml") 
-def model_cohort(config_file):
+def model_cohort(config):
     """
     model_cohort function load the clean data and run the regression
     to study the effects of regulation on firm exit rates
     Args:
-        config_file [str]: path to config file
+        config [str]: config file
     Returns:
         Final data
     """
 
     ####################
-    # Load data and config_file
+    # Load data
     ####################
-
-    config = parse_config(config_file)
-
+    
     # load data paths
-    cleaned_data_path = Path(config["model"]["cleaned_data_path"])
+    cleaned_data_path = Path(config["model"]["cohort_path"])
     results_tables_path = Path(config["model"]["results_tables_path"])
 
     df = pd.read_csv(Path.cwd()/cleaned_data_path)
@@ -44,8 +41,6 @@ def model_cohort(config_file):
     ####################
     # Regression
     ####################
-    
-    results_dict_1 = dict()
 
     for naics_curr in range(2, 5):
         # load data
@@ -56,9 +51,10 @@ def model_cohort(config_file):
         
         # regression
         data = data.set_index(['sector', 'cohort'])
-        data = data.loc[:, ['entry_rate_whole', 'death_rate_age', 'age_grp_dummy', 'year', 'log_incumbents_whole', 'log_gdp', 'log_gdp_cohort',
+        data = data.loc[:, ['entry_rate_whole', 'entry_rate_whole_cohort', 'death_rate', 'age_grp_dummy', 'year', 'log_gdp', 'log_gdp_cohort',
                             f'log_restriction_2_{naics_curr}', f'log_restriction_2_{naics_curr}_cohort']].dropna()
-        mod = PanelOLS.from_formula(formula = f'death_rate_age ~ log_restriction_2_{naics_curr} + log_restriction_2_{naics_curr}_cohort \
+        mod = PanelOLS.from_formula(formula = f'death_rate ~ log_restriction_2_{naics_curr} + log_restriction_2_{naics_curr}_cohort \
+                                                + entry_rate_whole + entry_rate_whole_cohort  \
                                                 + C(age_grp_dummy) + EntityEffects + TimeEffects', data = data, drop_absorbed=True)
         res = mod.fit(cov_type = 'heteroskedastic')
 
@@ -68,6 +64,83 @@ def model_cohort(config_file):
         file_path = Path.cwd()/results_tables_path/f"results_reg_naics_{naics_curr}.csv"
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(results.as_csv())
+            
+            
+def model_life_path(config):
+    """
+    model_life_path function load the clean data and run the regression
+    to study the effects of regulation on firm exit rates
+    Args:
+        config [str]: config file
+    Returns:
+        Final data
+    """
+    ####################
+    # Load data
+    ####################
 
+    # load data paths
+    cleaned_data_path = Path(config["model"]["life_path_path"])
+    results_tables_path = Path(config["model"]["results_tables_path"])
+
+    df = pd.read_csv(Path.cwd()/cleaned_data_path)  
+    
+    for age in range(1, 6):
+        # load data
+        data = df
+        
+        # sample restriction
+        data = data[data.year > 1985]
+        data = data[data.age_grp_dummy == age]
+        
+        # regression
+        data = data.set_index(['sector', 'year'])
+        
+        # create exogenous variable (list)
+        exo_var_list = []
+        for lags in range(0, age + 1):
+            exo_var_list.append(f"L_{lags}_log_restriction_2_0")
+            exo_var_list.append(f"L_{lags}_entry_rate_whole")
+            exo_var_list.append(f"L_{lags}_log_gdp")
+        
+        sample_data = data.loc[:, ["death_rate"] + exo_var_list].dropna()
+        exo_vars = sm.add_constant(sample_data[exo_var_list])
+        
+        # regression
+        mod = PanelOLS(sample_data.death_rate, exo_vars, entity_effects=True, time_effects=True)
+        res = mod.fit(cov_type = 'heteroskedastic')
+
+        # results
+        results = res.summary
+
+        file_path = Path.cwd()/results_tables_path/f"results_path_age_{age}.csv"
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(results.as_csv())
+        
+        
+        
+@click.command()
+@click.argument("config_file", type=str, default="src/config.yaml") 
+def model_output(config_file):
+    """
+    model_output function output results
+    Args:
+        config_file [str]: path to config file
+    Returns:
+        Final data
+    """
+
+    ####################
+    # Load data and config file
+    ####################
+    config = parse_config(config_file)
+    
+    
+    ####################
+    # Output
+    ####################
+    model_cohort(config)
+    model_life_path(config)
+    
 if __name__ == "__main__":
-    model_cohort()
+    model_output()
